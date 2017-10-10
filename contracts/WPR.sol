c4bfc1bd6e7467ffed5e357b3833b8ff924dd8d7
Klaus Hott Vidal <klahott@gmail.com>
2017-10-10 17:21:48 +1300

    WIP added open-zeppelin and modified Contribution to work with it.
    TODO: veting Contracts

---
+++ contracts/WPR.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.15;
+
+import "zeppelin-solidity/contracts/token/MintableToken.sol";
+
+/**
+ * @title WePower Contribution Token
+ */
+contract WPR is MintableToken {
+  string constant public name = "ExampleToken";
+  string constant public symbol = "EGT";
+  uint constant public decimals = 18;
+
+  function WPR() {
+  }
+
+  //////////
+  // Safety Methods
+  //////////
+
+  /// @notice This method can be used by the controller to extract mistakenly
+  ///  sent tokens to this contract.
+  /// @param _token The address of the token contract that you want to recover
+  ///  set to 0 in case you want to extract ether.
+  function claimTokens(address _token) onlyOwner {
+    if (_token == 0x0) {
+      owner.transfer(this.balance);
+      return;
+    }
+
+    ERC20 token = ERC20(_token);
+    uint balance = token.balanceOf(this);
+    token.transfer(owner, balance);
+    ClaimedTokens(_token, owner, balance);
+  }
+
+  event ClaimedTokens(address indexed _token, address indexed _controller, uint _amount);
+}
