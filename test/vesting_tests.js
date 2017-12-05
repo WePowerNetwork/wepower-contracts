e09da2d282f914346ea57f9313fbf1fcc394efea
Klaus Hott Vidal <klahott@gmail.com>
2017-12-05 17:59:15 +1300

    Fixing tests and Contribution bug

--- test/vesting_tests.js
+++ test/vesting_tests.js
@@ -11,7 +11,7 @@ const assert = require("chai").assert;
 const BigNumber = web3.BigNumber;
 import { expectThrow, duration, latestBlock, getTime } from "./utils.js";
 
-contract("Contribution", ([miner, owner, investor]) => {
+contract("Vesting", ([miner, owner, investor]) => {
   let wpr;
   let contribution;
   let exchanger;
