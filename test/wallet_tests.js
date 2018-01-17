cd863810f9b88e8a58d0cdb7b645581726972f14
Matt <sifrawr@gmail.com>
2018-01-17 16:55:48 +1300

    Updating tests for new InvestorWallet code

--- test/wallet_tests.js
+++ test/wallet_tests.js
@@ -1,6 +1,15 @@
+"use strict";
+
 const InvestorWallet = artifacts.require("MockInvestorWallet.sol");
 const InvestorWalletFactory = artifacts.require("InvestorWalletFactory.sol");
+const WCT = artifacts.require("WCT.sol");
+const WCT1 = artifacts.require("WCT1.sol");
 const WCT2 = artifacts.require("WCT2.sol");
+const WPR = artifacts.require("WPR.sol");
+const Exchanger = artifacts.require("MockExchanger.sol");
+const FutureTokenHolder = artifacts.require("FutureTokenHolder");
+const TeamTokenHolder = artifacts.require("TeamTokenHolder");
+const MockContribution = artifacts.require("MockContribution.sol");
 const MiniMeTokenFactory = artifacts.require("MiniMeTokenFactory");
 const assert = require("chai").assert;
 const BigNumber = web3.BigNumber;
@@ -9,32 +18,111 @@ import { expectThrow, duration, latestBlock, getTime } from "./utils.js";
 contract("InvestorWallet", ([wePower, investor]) => {
   let investorWallet;
   let investorWalletFactory;
+  let wct;
+  let wct1;
   let wct2;
+  let tokensPreSold = new BigNumber(50 * 10 ** 18);
+  let wpr;
+  let contribution;
+  let exchanger;
+  let walletFactory;
   let tokenFactory;
   let currentTime;
+  let multiSig = wePower;
+  let totalCap;
+  let bonusCap;
+  let collectorWeiCap;
+  let futureHolder;
+  let teamHolder;
+  let _communityHolder;
+  let latestBlockNumber;
 
   describe("#initialize", async function() {
     before(async function() {
       tokenFactory = await MiniMeTokenFactory.new();
     });
+
     beforeEach(async function() {
+      wct = await WCT.new(tokenFactory.address);
+      wct1 = await WCT1.new(tokenFactory.address);
       wct2 = await WCT2.new(tokenFactory.address);
+      await wct.generateTokens(wePower, tokensPreSold);
+      wpr = await WPR.new(tokenFactory.address);
+      contribution = await MockContribution.new(wpr.address);
+      exchanger = await Exchanger.new(
+        wct.address,
+        wct1.address,
+        wct2.address,
+        wpr.address,
+        contribution.address
+      );
+      walletFactory = await InvestorWalletFactory.new(wct2.address);
+      await wct2.changeController(walletFactory.address);
+
+      futureHolder = await FutureTokenHolder.new(
+        wePower,
+        contribution.address,
+        wpr.address
+      );
+      teamHolder = await TeamTokenHolder.new(
+        wePower,
+        contribution.address,
+        wpr.address
+      );
+      _communityHolder = "0x0039F22efB07A647557C7C5d17854CFD6D489eF3";
+      totalCap = new BigNumber(5 * 10 ** 18); // 5 eth
+      bonusCap = totalCap.div(10);
+      currentTime = getTime();
+      latestBlockNumber = await latestBlock();
+
+      await contribution.setBlockTimestamp(currentTime);
+      await contribution.setBlockNumber(latestBlockNumber);
+      await wpr.transferOwnership(contribution.address);
+      await walletFactory.setExchanger(exchanger.address);
+
+      await contribution.initialize(
+        wct.address,
+        wct1.address,
+        wct2.address,
+        exchanger.address,
+        multiSig,
+        futureHolder.address,
+        teamHolder.address,
+        _communityHolder,
+        bonusCap,
+        totalCap,
+        currentTime + 1,
+        currentTime + 10
+      );
+      currentTime = getTime();
+      latestBlockNumber = await latestBlock();
+      await contribution.setBlockTimestamp(currentTime + 2);
+      await contribution.setBlockNumber(latestBlockNumber + 1);
     });
 
     it("let's the investor take the tokens after the period", async function() {
-      let investorWallet = await InvestorWallet.new(wct2.address, 5, {
-        from: investor
-      });
+      let transaction = await walletFactory.createInvestorWallet(
+        wpr.address,
+        5,
+        investor,
+        1000
+      );
+
+      investorWallet = InvestorWallet.at(transaction.receipt.logs[0].address);
+
       currentTime = await getTime();
-      await wct2.generateTokens(investorWallet.address, 5 * 10 ** 18);
       let investorBalance = await wct2.balanceOf(investor);
       assert.equal(investorBalance.toNumber(), 0);
       await investorWallet.setBlockTimestamp(currentTime + duration.months(4));
-      await expectThrow(investorWallet.collectTokens({ from: investor }));
+      await exchanger.setBlockTimestamp(currentTime + duration.months(4));
+      await expectThrow(async () => { await investorWallet.collectTokens({ from: investor }) });
       await investorWallet.setBlockTimestamp(
         currentTime + duration.months(5) + duration.days(1)
       );
-      await expectThrow(investorWallet.collectTokens({ from: wePower }));
+      await exchanger.setBlockTimestamp(
+        currentTime + duration.months(5) + duration.days(1)
+      );
+      await expectThrow(async () => { await investorWallet.collectTokens({ from: wePower }) });
       await investorWallet.collectTokens({ from: investor });
       investorBalance = await wct2.balanceOf(investor);
       assert.equal(investorBalance.toNumber(), 5 * 10 ** 18);
