2c1c3645ace4287404e464e911a08fb5bcd31aed
Andres Gonzalez <andres@3months.com>
2018-01-23 09:46:20 +1300

    fix coverage

--- truffle.js
+++ truffle.js
@@ -6,7 +6,14 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*" // eslint-disable-line camelcase
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*", // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
     }
   }
 };
