db91ec2335bce0e3a0f426cf5a653992276bdbe2
Andres Gonzalez <andres@3months.com>
2018-01-24 21:17:20 +1300

    fixed solcover script, removed helper contracts from report

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,7 @@ require("babel-register");
 require("babel-polyfill");
 
 module.exports = {
-  copyPackages: ["zeppelin-solidity"],r
+  copyPackages: ["zeppelin-solidity"],
+  skipFiles: ["ERC20.sol", "MiniMeToken.sol", "SafeMath.sol", "WCT.sol"],
   norpc: true
 };
