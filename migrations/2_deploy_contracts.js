503210a9b45858b7f280b00747068b6d3456d502
Matt <sifrawr@gmail.com>
2018-01-24 14:37:04 +1300

    working on deploy script. debugging investor wallet bug

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,52 @@
-// var MiniMeTokenFactory = artifacts.require("MiniMeTokenFactory");
-// var WIT = artifacts.require("WP");
-// var SafeMath = artifacts.require("SafeMath");
-// var PreSale = artifacts.require("PreSale");
+var SafeMath = artifacts.require("SafeMath");
+var MiniMeTokenFactory = artifacts.require("MiniMeTokenFactory");
+var WCT2 = artifacts.require("WCT2");
+var InvestorWalletFactory = artifacts.require("InvestorWalletFactory");
+var WCT1 = artifacts.require("WCT1");
+var WPR = artifacts.require("WPR");
+var Contribution = artifacts.require("Contribution");
+var Exchanger = artifacts.require("Exchanger");
+var TeamTokenHolder = artifacts.require("TeamTokenHolder");
+var FutureTokenHolder = artifacts.require("FutureTokenHolder");
 
-module.exports = async function(deployer) {
-  //   awwit deployer.deploy(MiniMeTokenFactory);
-  //   awwit deployer.deploy(WIT, MiniMeTokenFactory.address);
-  //   deployer.deploy(SafeMath);
-  //   deployer.link(SafeMath, PreSale);
-  //   awwit deployer.deploy(PreSale, WIT.address);
-  //   WIT.deployed().changeController(PreSale.address);
+/*/var dateStart = Math.floor(new Date().getTime()/1000); // starts in 5 days
+console.log(dateStart);
+var dateEnd = dateStart + 10*24*60*60; // lasts 10 days
+var owner = "0x0019810eAceA494E393daf6D2340092b89c97eBB";
+/*/
+module.exports = function(deployer) {
+  return deployer.then(function(){
+    // deploy SafeMath
+   return deployer.deploy(SafeMath);
+  }).then(function() {
+    // link SafeMath
+  return deployer.link(SafeMath, [Contribution, Exchanger, FutureTokenHolder, InvestorWalletFactory, TeamTokenHolder, WPR]);
+  }).then(function(){
+    // deploy MiniMeTokenFactory first
+    return deployer.deploy(MiniMeTokenFactory);
+  }).then(function(){
+    // then WCT2
+    return deployer.deploy(WCT2, MiniMeTokenFactory.address);
+  }).then(function(){
+    // then InvestorWalletFactory
+    return deployer.deploy(InvestorWalletFactory, WCT2.address);
+  }).then(function(){
+    // then WCT1
+    return deployer.deploy(WCT1, MiniMeTokenFactory.address);
+  }).then(function(){
+    // then WPR
+    return deployer.deploy(WPR);
+  }).then(function(){
+    // then Contribution
+    return deployer.deploy(Contribution, WPR.address);
+  }).then(function(){
+    // then Exchanger
+    return deployer.deploy(Exchanger, WCT1.address, WCT2.address, WPR.address, Contribution.address);
+  }).then(function(){
+    // then TeamTokenHolder
+    return deployer.deploy(TeamTokenHolder, deployer, Contribution.address, WPR.address);
+  }).then(function(){
+    // then FutureTokenHolder
+    return deployer.deploy(FutureTokenHolder, deployer, Contribution.address, WPR.address);
+  });
 };
